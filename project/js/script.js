2381914c5efaf304633fc93d5a7ab3d3b2618650
RomaRoma <v12nes11@gmail.com>
2021-07-16 16:34:54 +0300

    modified, w/o last tsk

--- project/js/script.js
+++ project/js/script.js
@@ -1,71 +1,96 @@
 'use strict';
 
-const movieDB = {
-    movies: [
-        "Логан",
-        "Лига справедливости",
-        "Ла-ла лэнд",
-        "Одержимость",
-        "Скотт Пилигрим против..."
-    ]
-};
+document.addEventListener('DOMContentLoaded', () => {
+    const movieDB = {
+        movies: [
+            "Логан",
+            "Лига справедливости",
+            "Ла-ла лэнд",
+            "Одержимость",
+            "Скотт Пилигрим против..."
+        ]
+    };
+    
+    // Variables 2 w3k w/
+    let adItems = document.querySelectorAll('.promo__adv img'),
+        background = document.querySelector('.promo__bg'),
+        heading = background.querySelector('.promo__genre'),
+        addForm = document.querySelector('form.add'),
+        filmList = document.querySelector('.promo__interactive-list'),
+        formInput = addForm.querySelector('.adding__input'),
+        formCheckbox = addForm.querySelector('[type="checkbox"'),
+        formSubmitButton = document.querySelector('.yes').nextElementSibling;    
+    // Deleting all the AD
+    const adRemove = (arr) => {
+        arr.forEach(item =>{
+            item.remove();
+        });   
+    };
+    adRemove(adItems);
+    
+    const makeChanges = (text, background) => {
+        text.innerHTML= 'Драма';
+        background.style.backgroundImage = 'url("img/bg.jpg")';
+    };
+    makeChanges(heading, background);
 
-// Variables 2 w3k w/
-let adItems = document.querySelectorAll('.promo__adv img'),
-    background = document.querySelector('.promo__bg'),
-    heading = background.querySelector('.promo__genre'),
-    filmList = document.querySelector('.promo__interactive-list'),
-    formInput = document.querySelector('.adding__input'),
-    formYesCheckbox = document.querySelector('.yes'),
-    formSubmitButton = document.querySelector('.yes').nextElementSibling,
-    inputValue = formInput.value;
+    const sortArr = (arr) => {
+        arr.sort();
+    };
+    sortArr(movieDB.movies);
 
-// Deleting all the AD
-adItems.forEach(item =>{
-    item.remove();
-});
+    const formRefactor = (films, parent) => {
+        // let moviesArr = movieDB.movies.map(item => item.toUpperCase()); // 1.
+        parent.innerHTML = ''; // 3.
+        
+        films.forEach((item, i) => { // 4.
+            parent.innerHTML += `
+            <li class="promo__interactive-item">
+            ${i+1}. ${item}
+                <div class="delete"></div>
+            </li>   
+            `;
+        });
+        sortArr(movieDB.movies);
+    };
+    formRefactor(movieDB.movies, filmList);
 
-// Changing the header
-heading.innerHTML= 'Драма';
+    // Getting the value of input
+    formSubmitButton.addEventListener('click', (event)=>{
+        event.preventDefault(); // Preventing the page from reloading1.
+    
+        let inputValue = formInput.value;
+        let favorite = formCheckbox.checked;
 
-// Changing the BG
-background.style.backgroundImage = 'url("img/bg.jpg")';
-
-formRefactor();
-
-// Form basic behaviour
-// 1. Creating new uppercase arr.
-// 2. Sort arr.
-// 3. Clean the default
-// 4. Generate new html
-function formRefactor() {
-    let moviesArr = movieDB.movies.map(item => item.toUpperCase()); // 1.
-    moviesArr.sort(); // 2.
-    filmList.innerHTML = ''; // 3.
-
-    moviesArr.forEach((item, i) => { // 4.
-        filmList.innerHTML += `
-        <li class="promo__interactive-item">
-        ${i+1}. ${item}
-            <div class="delete"></div>
-        </li>   
-        `;
+        if (inputValue) {
+            if(inputValue.length>21) {
+                inputValue = `${inputValue.substring(0,21)}...`;
+            }
+            if (favorite) {
+                console.log('adding the new movie');
+            }
+            movieDB.movies.push(inputValue);
+            sortArr(movieDB.movies);
+            formRefactor(movieDB.movies, filmList);
+            console.log(movieDB.movies);
+        }
+        // formRefactor(); // Repeating the sorting, cleaning and data generating.
     });
-}
 
 
-// Getting the value of input
-formSubmitButton.addEventListener('click', (event)=>{
-    event.preventDefault(); // Preventing the page from updating.
+    document.querySelectorAll('.delete').forEach((item, i) => {
+        // console.log(item);x
+           item.addEventListener('click', () => {  
+                // item.closest(".promo__interactive-item").remove();
+                // movieDB.movies.splice(i, 1);       
+                // item.remove();
 
-    let inputValue = formInput.value;
-
-    if(inputValue.length>21){
-        let newInputValue = inputValue.substring(0, 21) + "..."; // Cropping it.
-        movieDB.movies.push(newInputValue);// Push new inputValue.
-    } else {
-        movieDB.movies.push(inputValue); // Push inputValue standart.
-    }
-
-    formRefactor(); // Repeating the sorting, cleaning and data generating.
+                console.log(item);
+                console.log(item.parentElement);
+                console.log(`${item} - item`);
+                console.log(`${item.parentElement} - item parent`);          
+                // console.log(item.closest);
+           });
+    });
+    
 });
